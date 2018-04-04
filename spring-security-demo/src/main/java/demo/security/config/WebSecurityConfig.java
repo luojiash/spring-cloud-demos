9491ec6c32645af05f7a8d154e10816d7d398aa3
luojiash <52547071@qq.com>
2018-04-04 17:46:35 +0800

    spring security init

---
+++ spring-security-demo/src/main/java/demo/security/config/WebSecurityConfig.java
@@ -0,0 +1,52 @@
+package demo.security.config;
+
+import demo.security.MyAuthenticationFailureHandler;
+import demo.security.MyAuthenticationSuccessHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user").password("password").roles("USER").build());
+        return manager;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .withObjectPostProcessor(new ObjectPostProcessor<UsernamePasswordAuthenticationFilter>() {
+                    @Override
+                    public <O extends UsernamePasswordAuthenticationFilter> O postProcess(O object) {
+                        object.setAuthenticationSuccessHandler(new MyAuthenticationSuccessHandler());
+                        object.setAuthenticationFailureHandler(new MyAuthenticationFailureHandler());
+                        return object;
+                    }
+                })
+        ;
+    }
+
+    // customize PasswordEncoder
+//    @Bean
+//    public PasswordEncoder encoder() {
+//        return new BCryptPasswordEncoder(10, new SecureRandom());
+//    }
+}
